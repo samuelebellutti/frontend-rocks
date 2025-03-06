46d5de6f60037d559dad022436cbaa7a2e384457
samuelebellutti <sambellu334@gmail.com>
2025-03-06 15:17:08 +0000

    pokedex3

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -8,7 +8,7 @@ export const AppRouter = ()=>{
     <BrowserRouter>
     <Routes>
         <Route path= "/frontend-rocks" element={<RootRoute/>}/>
-        <Route path= "/frontend-rocks/dettaglio/:id" element={<DetailRoute/>}/>
+        <Route path= "/frontend-rocks/dettaglio/:id"  element={<DetailRoute/>}/>
 
 
     </Routes>
