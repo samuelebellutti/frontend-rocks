8d509c66d06560bc132e2ad4070fe44d712d6629
samuelebellutti <sambellu334@gmail.com>
2025-03-06 11:27:59 +0000

    change

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export const App = () => {
 
         <div className="flex flex-col items-center space-y-4">
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-white -600 transition-colors"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue -600 transition-colors"
             onClick={() => setCount((count) => count + 1)}
           >
             Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
